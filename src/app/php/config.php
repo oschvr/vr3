60a607f0482a48fe8e9d0412560acc82d605c7a2
Roy <kdrums12@gmail.com>
2016-07-21 15:35:36 -0500

    PHP files contact and popData

---
+++ src/app/php/config.php
@@ -0,0 +1,43 @@
+<?php
+	define("__HOST__", "localhost");
+	define("__USER__", "root");
+	define("__PASS__", "");
+	define("__BASE__", "project");
+
+	class DB {
+		private $con = false;
+		private $data = array();
+
+		public function __construct() {
+			$this->con = new mysqli(__HOST__, __USER__, __PASS__, __BASE__);
+
+			if(mysqli_connect_errno()) {
+				die("DB connection failed:" . mysqli_connect_error());
+			}
+		}
+
+		public function qryPop() {
+			$sql = "SELECT * FROM `events` ORDER BY `id` DESC";
+			$qry = $this->con->query($sql);
+			if($qry->num_rows > 0) {
+				while($row = $qry->fetch_object()) {
+					$this->data[] = $row;
+				}
+			} else {
+				$this->data[] = null;
+			}
+			$this->con->close();
+		}
+
+		public function qryFire($sql=null) {
+			if($sql == null) {
+				$this->qryPop();
+			} else {
+				$this->con->query($sql);
+				$this->qryPop();
+			}
+		//	$this->con->close();
+			return $this->data;
+		}
+	}
+?>
